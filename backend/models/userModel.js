7bb65a439bf54b95fc884ef8493fc6af9447f7b1
Nikhil Singh <nikhilbangalor@gmail.com>
2021-10-18 09:45:47 +0530

    password encryption and error handling

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -32,6 +32,17 @@ const userSchema = mongoose.Schema(
   { timestamps: true }
 );
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword=async function (enteredPassword)
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
