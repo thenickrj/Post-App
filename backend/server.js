1b43c9fbcdb463cddcedf98af4c2e2ec5d243600
Nikhil Singh <nikhilbangalor@gmail.com>
2021-10-18 18:08:23 +0530

    login and register frontend functionality start

--- backend/server.js
+++ backend/server.js
@@ -30,4 +30,4 @@ app.get("/api/notes/:id", (req, res) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, console.log(`Server started on port ${5000}`));
+app.listen(port, console.log(`Server started on port ${port}`));
--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Footer from "./components/Footer/Footer";
 import LandingPage from "./Screens/LandingPage/LandingPage";
 import { BrowserRouter, Route } from "react-router-dom";
 import MyNotes from "./Screens/MyNotes/MyNotes";
+import LoginScreen from "./Screens/LoginScreen/LoginScreen";
+import RegisterScreen from "./Screens/RegisterScreen/RegisterScreen";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <BrowserRouter>
         <Header />
         <Route exact path="/" component={LandingPage} />
+        <Route exact path="/login" component={LoginScreen} />
+        <Route exact path="/register" component={RegisterScreen} />
         <Route path="/mynotes" component={() => <MyNotes />} />
         <Footer />
       </BrowserRouter>
